782bab50210923033a082cc83deedabff8580677
rickf <rickf@OCULUS-ONE>
2022-11-17 10:52:48 -0800

    Add project files.

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("     |     |     ");
+Console.WriteLine("  {0}  |  {1}  |  {2} ",1,2,3);
+Console.WriteLine("-----|-----|-----");
+Console.WriteLine("  {0}  |  {1}  |  {2} ", 4, 5, 6);
+Console.WriteLine("-----|-----|-----");
+Console.WriteLine("  {0}  |  {1}  |  {2} ", 7, 8, 9);
+Console.WriteLine("     |     |     ");
