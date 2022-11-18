875df6b5edf0b6c521870cd11a955331f60003c4
rickf <rickf@OCULUS-ONE>
2022-11-17 22:08:41 -0800

    partial... need win test and key test

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,8 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("     |     |     ");
-Console.WriteLine("  {0}  |  {1}  |  {2} ",1,2,3);
-Console.WriteLine("-----|-----|-----");
-Console.WriteLine("  {0}  |  {1}  |  {2} ", 4, 5, 6);
-Console.WriteLine("-----|-----|-----");
-Console.WriteLine("  {0}  |  {1}  |  {2} ", 7, 8, 9);
-Console.WriteLine("     |     |     ");
+﻿using static TicTacPlayGrid;
+
+public class Program    
+{
+    public static void Main(string[] args)
+    {
+        int player = 2;
+        int input = 0;
+        bool inputCorrect = true;
+
+        TicTacPlayGrid.PlayGrid();
+             
+
+        do
+        {
+            if(player == 2)
+                player = 1;
+            else if(player == 1)
+                player = 2;
+
+            input = Convert.ToInt32(Console.ReadLine());
+
+            switch (player)
+            {
+                case 1: //Player 1 turn
+                    {
+                        switch (input)
+                        {
+                            case 1: playfield[0, 0] = 'x'; break;
+                            case 2: playfield[0, 1] = 'x'; break;
+                            case 3: playfield[0, 2] = 'x'; break;
+                            case 4: playfield[1, 0] = 'x'; break;
+                            case 5: playfield[1, 1] = 'x'; break;
+                            case 6: playfield[1, 2] = 'x'; break;
+                            case 7: playfield[2, 0] = 'x'; break;
+                            case 8: playfield[2, 1] = 'x'; break;
+                            case 9: playfield[2, 2] = 'x'; break;
+                        }
+                        break;
+
+                    }
+                case 2: //Player 2 turn
+                    {
+                        switch (input)
+                        {
+                            case 1: playfield[0, 0] = 'o'; break;
+                            case 2: playfield[0, 1] = 'o'; break;
+                            case 3: playfield[0, 2] = 'o'; break;
+                            case 4: playfield[1, 0] = 'o'; break;
+                            case 5: playfield[1, 1] = 'o'; break;
+                            case 6: playfield[1, 2] = 'o'; break;
+                            case 7: playfield[2, 0] = 'o'; break;
+                            case 8: playfield[2, 1] = 'o'; break;
+                            case 9: playfield[2, 2] = 'o'; break;
+                        }
+                        break;
+                    }
+            }
+            PlayGrid();
+        }while(true);
+
+
+    
+
+
+
+
+
+
+    }
+}
---
+++ TicTacToe/TicTacPlayGrid.cs
@@ -0,0 +1,25 @@
+﻿
+
+public class TicTacPlayGrid
+{
+    public static char[,] playfield =
+    {
+        {'1','2','3'},
+        {'4','5','6'},
+        {'7','8','9'}
+    };
+    public static void PlayGrid()
+    {
+        Console.WriteLine("     |     |     ");
+        Console.WriteLine("  {0}  |  {1}  |  {2} ", playfield[0, 0], playfield[0, 1], playfield[0,2]);
+        Console.WriteLine("-----|-----|-----");
+        Console.WriteLine("  {0}  |  {1}  |  {2} ", playfield[1, 0], playfield[1, 1], playfield[1, 2]);
+        Console.WriteLine("-----|-----|-----");
+        Console.WriteLine("  {0}  |  {1}  |  {2} ", playfield[2, 0], playfield[2, 1], playfield[2, 2]);
+        Console.WriteLine("     |     |     ");
+    }
+
+    
+
+
+}
